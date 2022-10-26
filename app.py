2b9b3534a7c13a91b1225c23e9f75eb4c89a8d49
Sameer Alam <sameeralamynwa@gmail.com>
2022-10-26 15:36:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -149,4 +149,4 @@ def predict_allergies():
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 33507))
-    app.run(debug=True, port)
+    app.run(debug=True, port, host='0.0.0.0')
