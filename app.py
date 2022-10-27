4c977cdc93949741574a6224d02166e2b78926b7
Sameer Alam <sameeralamynwa@gmail.com>
2022-10-27 23:18:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ from gensim.parsing.preprocessing import remove_stopwords
 from nltk.stem import PorterStemmer  
 
 app = Flask(__name__)
-conditions = pickle.load(open('conditions.pkl', 'rb'))
+
 allergies = pickle.load(open('allergies.pkl', 'rb'))
+conditions = pickle.load(open('conditions', 'rb'))
 
 @app.route('/')
 def home():
@@ -16,6 +17,11 @@ def home():
 
 @app.route('/predict_conditions',methods=['POST'])
 def predict_conditions():
+    print(conditions.get_xgb_params())
+    x = conditions.get_booster().feature_names
+    print(len(x))
+    print(x)
+    print("sameer")
     result = request.form.to_dict(flat=False)
     print(result)
     int_features = []
@@ -40,55 +46,27 @@ def predict_conditions():
     conditions_description_split = conditions_description.split()
     print(conditions_description_split)
     
-    descriptions_columns = ['19', '30', '40', 'abus', 'access', 'acquir', 'activ', 'acut', 'aeruginosa', 
-                            'alcohol', 'allerg', 'allergi', 'alzheimer', 'amput', 'anemia', 'ankl', 'antepartum', 
-                            'anterior', 'antibodi', 'anu', 'anxieti', 'apnea', 'append', 'appendectomi', 'appendix', 
-                            'arm', 'arrest', 'arthriti', 'associ', 'asthma', 'atop', 'atrial', 'atroph', 'attent', 'aura', 
-                            'bacteremia', 'bacteri', 'behavior', 'bifida', 'bleed', 'blight', 'bodi', 'brain', 'breast', 'bronchiti', 
-                            'bullet', 'burn', 'cancer', 'carcinoma', 'cardiac', 'caus', 'cell', 'cerebr', 'certif', 'chiari', 'child', 
-                            'childhood', 'chill', 'cholecyst', 'cholelithiasi', 'chronic', 'clavicl', 'close', 'coagul', 'coli', 'collater', 
-                            'colon', 'column', 'complic', 'concuss', 'congenit', 'congest', 'conjunctiv', 'conscious', 'constip', 'contact', 
-                            'cord', 'coronari', 'cough', 'covid', 'crimin', 'cruciat', 'cuff', 'cystic', 'cystiti', 'daili', 'damag', 'deep', 
-                            'defici', 'deficit', 'deform', 'degre', 'depress', 'dermat', 'diabet', 'diarrhea', 'die', 'disabl', 'diseas', 'disloc', 
-                            'disord', 'disorder', 'distress', 'dribbl', 'drink', 'drug', 'dyspnea', 'dystonia', 'earli', 'eclampsia', 'edema', 'educ', 
-                            'embol', 'emphysema', 'employ', 'environ', 'epilepsi', 'episod', 'equival', 'escherichia', 'excess', 'facial', 'failur', 
-                            'famili', 'fatigu', 'febril', 'femal', 'fetu', 'fever', 'fibril', 'fibromyalgia', 'fibrosi', 'find', 'finding', 'first', 
-                            'foot', 'forc', 'forearm', 'fractur', 'full', 'gastroesophag', 'gondii', 'gout', 'ha', 'hand', 'headach', 'heart', 'high', 
-                            'higher', 'hip', 'histori', 'homeless', 'hospic', 'hous', 'human', 'hydrocephalu', 'hyperglycemia', 'hyperlipidemia', 'hypertens', 
-                            'hypertriglyceridemia', 'hypothyroid', 'hypoxemia', 'idiopath', 'ii', 'immun', 'immunodefici', 'impact', 'index', 'infarct', 'infect', 
-                            'infecti', 'infertil', 'injuri', 'intellectu', 'intim', 'intract', 'involv', 'isol', 'joint', 'kidney', 'knee', 'labor', 'lacer', 
-                            'lack', 'latex', 'lesion', 'leukemia', 'ligament', 'limb', 'limit', 'local', 'loss', 'low', 'lower', 'lung', 'macular', 
-                            'major', 'male', 'malform', 'malign', 'mass', 'media', 'medial', 'mellitu', 'meningomyelocel', 'meniscu', 'metabol', 'metastasi', 
-                            'microalbuminuria', 'migrain', 'migrant', 'miscarriag', 'misus', 'molar', 'mouth', 'muscl', 'myeloid', 'myocardi', 'nasal', 'nausea', 
-                            'neck', 'neoplasm', 'neuropathi', 'neutropenia', 'non', 'nonprolif', 'normal', 'not', 'obes', 'obstruct', 'occulta', 'onli', 'onset', 
-                            'opioid', 'osteoarthr', 'osteoporosi', 'otiti', 'overdos', 'overlap', 'ovum', 'pain', 'palsi', 'panic', 'paralysi', 'part', 'partner', 
-                            'passiv', 'patellar', 'patholog', 'perenni', 'person', 'pharyng', 'pneumonia', 'polyp', 'poor', 'posit', 'prediabet', 'preeclampsia', 
-                            'pregnanc', 'pressur', 'primari', 'problem', 'prolif', 'prostat', 'proteinuria', 'protract', 'pseudomona', 'pulmonari', 'pyelonephr', 
-                            'reaction', 'receiv', 'record', 'rectal', 'recurr', 'reflux', 'refuge', 'renal', 'report', 'respiratori', 'retinopathi', 'rheumatoid', 
-                            'rhiniti', 'rib', 'risk', 'rotat', 'ruptur', 'saliv', 'saliva', 'school', 'season', 'second', 'secondari', 'seizur', 'sepsi', 'septic',
-                            'serv', 'sever', 'shock', 'shoulder', 'singl', 'sinus', 'situ', 'situation', 'sleep', 'smal', 'small', 'smoke', 'social', 'sore', 'spastic', 
-                            'spina', 'spinal', 'sprain', 'sputum', 'stage', 'streptococc', 'stress', 'stroke', 'sublux', 'suspect', 'swallow', 'symptom', 'syndrom', 
-                            'tast', 'tear', 'tendon', 'thigh', 'third', 'throat', 'thrombosi', 'tim', 'tnm', 'tobacco', 'tone', 'toxoplasma', 'tract', 'transform', 
-                            'transport', 'traumat', 'trimest', 'tubal', 'tumor', 'type', 'ulcer', 'unabl', 'unemploy', 'unhealthi', 'unknown', 'unsatisfactori', 'urinari', 
-                            'variat', 'venou', 'vertebr', 'victim', 'violenc', 'viral', 'viru', 'vomit', 'wheez', 'whiplash', 'wound', 'wrist']
-    
-    
+    descriptions_columns = ['19', '30', '40', 'abus', 'access', 'acquir', 'activ', 'acut', 'aeruginosa', 'alcohol', 'allerg', 'allergi', 'alzheimer', 'amput', 'anemia', 'ankl', 'antepartum', 'anterior', 'antibodi', 'anu', 'anxieti', 'apnea', 'append', 'appendectomi', 'appendix', 'arm', 'arrest', 'arthriti', 'associ', 'asthma', 'atop', 'atrial', 'atroph', 'attent', 'aura', 'bacteremia', 'bacteri', 'behavior', 'bifida', 'bleed', 'blight', 'bodi', 'brain', 'breast', 'bronchiti', 'bullet', 'burn', 'cancer', 'carcinoma', 'cardiac', 'caus', 'cell', 'cerebr', 'certif', 'chiari', 'child', 'childhood', 'chill', 'cholecyst', 'cholelithiasi', 'chronic', 'clavicl', 'close', 'coagul', 'coli', 'collater', 'colon', 'column', 'complic', 'concuss', 'congenit', 'congest', 'conjunctiv', 'conscious', 'constip', 'contact', 'cord', 'coronari', 'cough', 'covid', 'crimin', 'cruciat', 'cuff', 'cystic', 'cystiti', 'daili', 'damag', 'deep', 'defici', 'deficit', 'deform', 'degre', 'depress', 'dermat', 'diabet', 'diarrhea', 'die', 'disabl', 'diseas', 'disloc', 'disord', 'disorder', 'distress', 'dribbl', 'drink', 'drug', 'dyspnea', 'dystonia', 'earli', 'eclampsia', 'edema', 'educ', 'embol', 'emphysema', 'employ', 'environ', 'epilepsi', 'episod', 'equival', 'escherichia', 'excess', 'facial', 'failur', 'famili', 'fatigu', 'febril', 'femal', 'fetu', 'fever', 'fibril', 'fibromyalgia', 'fibrosi', 'find', 'finding', 'first', 'foot', 'forc', 'forearm', 'fractur', 'full', 'gastroesophag', 'gondii', 'gout', 'ha', 'hand', 'headach', 'heart', 'high', 'higher', 'hip', 'histori', 'homeless', 'hospic', 'hous', 'human', 'hydrocephalu', 'hyperglycemia', 'hyperlipidemia', 'hypertens', 'hypertriglyceridemia', 'hypothyroid', 'hypoxemia', 'idiopath', 'ii', 'immun', 'immunodefici', 'impact', 'index', 'infarct', 'infect', 'infecti', 'infertil', 'injuri', 'intellectu', 'intim', 'intract', 'involv', 'isol', 'joint', 'kidney', 'knee', 'labor', 'lacer', 'lack', 'latex', 'lesion', 'leukemia', 'ligament', 'limb', 'limit', 'local', 'loss', 'low', 'lower', 'lung', 'macular', 'major', 'male', 'malform', 'malign', 'mass', 'media', 'medial', 'mellitu', 'meningomyelocel', 'meniscu', 'metabol', 'metastasi', 'microalbuminuria', 'migrain', 'migrant', 'miscarriag', 'misus', 'molar', 'mouth', 'muscl', 'myeloid', 'myocardi', 'nasal', 'nausea', 'neck', 'neoplasm', 'neuropathi', 'neutropenia', 'non', 'nonprolif', 'normal', 'not', 'obes', 'obstruct', 'occulta', 'onli', 'onset', 'opioid', 'osteoarthr', 'osteoporosi', 'otiti', 'overdos', 'overlap', 'ovum', 'pain', 'palsi', 'panic', 'paralysi', 'part', 'partner', 'passiv', 'patellar', 'patholog', 'perenni', 'person', 'pharyng', 'pneumonia', 'polyp', 'poor', 'posit', 'prediabet', 'preeclampsia', 'pregnanc', 'pressur', 'primari', 'problem', 'prolif', 'prostat', 'proteinuria', 'protract', 'pseudomona', 'pulmonari', 'pyelonephr', 'reaction', 'receiv', 'record', 'rectal', 'recurr', 'reflux', 'refuge', 'renal', 'report', 'respiratori', 'retinopathi', 'rheumatoid', 'rhiniti', 'rib', 'risk', 'rotat', 'ruptur', 'saliv', 'saliva', 'school', 'season', 'second', 'secondari', 'seizur', 'sepsi', 'septic', 'serv', 'sever', 'shock', 'shoulder', 'singl', 'sinus', 'situ', 'situation', 'sleep', 'smal', 'small', 'smoke', 'social', 'sore', 'spastic', 'spina', 'spinal', 'sprain', 'sputum', 'stage', 'streptococc', 'stress', 'stroke', 'sublux', 'suspect', 'swallow', 'symptom', 'syndrom', 'tast', 'tear', 'tendon', 'thigh', 'third', 'throat', 'thrombosi', 'tim', 'tnm', 'tobacco', 'tone', 'toxoplasma', 'tract', 'transform', 'transport', 'traumat', 'trimest', 'tubal', 'tumor', 'type', 'ulcer', 'unabl', 'unemploy', 'unhealthi', 'unknown', 'unsatisfactori', 'urinari', 'variat', 'venou', 'vertebr', 'victim', 'violenc', 'viral', 'viru', 'vomit', 'wheez', 'whiplash', 'wound', 'wrist']    
+    print(f"the len is {len(descriptions_columns)}")
     for i in descriptions_columns:
         int_features.append(conditions_description_split.count(i))
     
-    print("int_features size" + len(int_features))
+    print(len(int_features))
     
     tot_features = patient_features + descriptions_columns
-    print("tot_feat")
-    print(tot_features)
+    print(len(tot_features))
     final_features = [np.array(int_features)]
+    print(final_features)
     df_features = pd.DataFrame(final_features, columns = tot_features)
     
     prediction = conditions.predict(df_features)
     print(prediction)
     output = prediction[0]
-
+        
+    print(tot_features)
+    print(len(tot_features))
     return render_template('index.html', prediction_text='ENCOUNTER CLASS IS {}'.format(output))
+
 
 @app.route('/predict_allergies',methods=['POST'])
 def predict_allergies():
